8f69bf56a4149151148146bcb15ded372653ddf1
Christopher Okokon Ntuk <engchris95@gmail.com>
2022-06-08 14:28:26 +0100

    More fixes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,11 @@
     <div class="col-lg-8">
       <h3 class="f24 fw-bold text-white">Add Cities</h3>
       <p class="f18 fw-normal text-white mt-1">Add 5 cities whose temperature you want to track.</p>
-      <form action="" class="weather-form mt-5">
+      <form action="" class="weather-form mt-5" @submit.prevent="handleSubmit()">
         <div class="form-group">
           <img src="@/assets/img/icons/icon-plus.svg" alt="add icon" class="buttons add-icon">
-          <input type="text" class="form-control" placeholder="Add a city..." />
-          <button class="btn add-city buttons">
+          <input type="text" class="form-control" placeholder="Add a city..." v-model="city" />
+          <button class="btn add-city buttons cursor-pointer">
             Add
           </button>
         </div>
@@ -25,9 +25,15 @@ export default {
   name: "HomeView",
   components: { WeatherList },
   data() {
-    return {};
+    return {
+      city: "",
+    };
   },
-  methods: {},
+  methods: {
+    handleSubmit() {
+      console.log("submit");
+    }
+  },
 };
 </script>
 
