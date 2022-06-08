05acf6303114ba325ed9aacb4ba4fe37d9e34f7c
Christopher Okokon Ntuk <engchris95@gmail.com>
2022-06-08 16:02:05 +0100

    Removed redundant codes

--- src/functions/actions/WeatherRequest.js
+++ src/functions/actions/WeatherRequest.js
@@ -7,12 +7,10 @@ export default {
     const response = WeatherService.addCity(credentials)
       .then((result) => {
         if (Object.keys(result.data).indexOf("success") == 0) {
-          console.log(1)
           const { success, error: { info } } = result.data;
           return { status: success, message: info };
         } else {
           store.dispatch("setCities", result.data);
-          console.log(result.data);
           return { status: true };
         }
       })
