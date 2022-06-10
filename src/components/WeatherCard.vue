9170e58b73268f1ce801456045a12231faf9ffca
Christopher Okokon Ntuk <engchris95@gmail.com>
2022-06-10 12:39:38 +0100

    Assessment completed 2

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="weather-card text-center">
-    <img src="@/assets/img/icons/close.png" alt="icon" class="close cursor-pointer" @click.prevent="removeCity()">
+    <img :src="deleteIcon" alt="icon" class="close cursor-pointer" @click.prevent="removeCity()" @mouseenter="toggleHover()" @mouseleave="toggleHover()">
     <h3 class="card-title text-primary m-0">{{ city.address && $globalFunc.capitalizeFirstLetter(city.address) }}</h3>
     <p class="sub-card-title text-grey f14 m-0">{{ city.resolvedAddress }}</p>
     <p class="temperature text-primary">
@@ -18,7 +18,20 @@ export default {
       required: true,
     },
   },
-  components: { },
+  components: {},
+  computed: {
+    deleteIcon() {
+      if (this.hover) {
+        return require(`@/assets/img/icons/close-red.png`)
+      }
+      return require(`@/assets/img/icons/close.png`)
+    }
+  },
+  data() {
+    return {
+      hover: false,
+    };
+  },
   methods: {
     viewMoreInfo() {
       this.$router.push({
@@ -33,7 +46,10 @@ export default {
       if (confirm("Are you sure you want to remove this city?")) {
         this.$store.dispatch("removeCity", this.city.id);
       }
-    }
+    },
+    toggleHover() {
+      this.hover = !this.hover;
+    },
   },
 };
 </script>
